bb0c15fbd1868ca5dc47cd4296d15e8b8cb2c9e5
10609436 <finley.williamson@tkelevator.com>
2021-10-05 16:19:11 -0400

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        indent: ['error', 4],
+        'import/prefer-default-export': 'off',
+        'global-require': 'off',
+    },
+};
